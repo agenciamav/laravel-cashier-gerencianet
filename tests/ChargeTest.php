350ecb290bd08a2ebeeea2297e6403860a7c4854
Luciano <tonetlds@gmail.com>
2016-07-17 13:38:06 -0300

    Tests deleting plans OK

--- tests/ChargeTest.php
+++ tests/ChargeTest.php
@@ -10,99 +10,7 @@ use Illuminate\Http\Request;
 class ChargeTest extends PHPUnit_Framework_TestCase
 {
 
-    private $options;
-    private $plans;
-
-    private $user;
-
-    public static function setUpBeforeClass()
-    {
-        if (file_exists(__DIR__.'/../.env')) {
-            $dotenv = new Dotenv\Dotenv(__DIR__.'/../');
-            $dotenv->load();
-        }
-    }
-
-    public function setUp()
-    {
-
-        Eloquent::unguard();
-
-        $db = new DB;
-        $db->addConnection([
-            'driver' => 'sqlite',
-            'database' => ':memory:',
-        ]);
-        $db->bootEloquent();
-        $db->setAsGlobal();
-
-        // Create users table
-        $this->schema()->create('users', function ($table) {
-            $table->increments('id');
-            $table->string('email');
-            $table->string('name');
-            $table->string('phone_number')->nullable();
-            $table->biginteger('cpf')->nullable();
-            $table->date('birth')->nullable();
-            $table->string('gerencianet_id')->nullable();
-            $table->string('card_brand')->nullable();
-            $table->string('card_last_four')->nullable();
-            $table->timestamps();
-        });
-
-        // Create subscriptions table
-        $this->schema()->create('subscriptions', function ($table) {
-
-            $table->increments('id');
-
-            $table->string('name')->nullable();
-            $table->integer('user_id');
-            $table->integer('subscription_id');
-            $table->string('status')->default("new");
-            $table->string('custom_id')->nullable();
-            $table->string('notification_url')->nullable();
-            $table->string('payment_method')->nullable();
-            $table->datetime('next_execution')->nullable();
-            $table->datetime('next_expire_at')->nullable();
-            $table->integer('plan_id')->nullable();
-            $table->integer('occurrences')->default(0);
-
-            // LOCAL TRIAL MANAGMENT
-            $table->datetime('trial_ends_at')->nullable();
-            $table->datetime('ends_at')->nullable();
-
-            $table->timestamps();
-
-        });
-
-        // ------------------------------------------------------
-
-        $faker = Faker\Factory::create('pt_BR');
-
-        $faker->addProvider(new \Faker\Provider\pt_BR\Person($faker));
-        $faker->addProvider(new \Faker\Provider\en_US\PhoneNumber($faker));
-
-        $user = User::create([
-            'email'        => $faker->email,
-            'name'         => $faker->name,
-            'phone_number' => $faker->regexify('[1-9]{2}9?[0-9]{8}'),
-            'cpf'          => $faker->cpf(false),
-            'birth'        => Carbon::now()->subYears(21)->format('Y-m-d')
-        ]);
-        $this->user = $user;
-
-        $GLOBALS['user'] = $user;
-    }
-
-
-    public function tearDown()
-    {
-
-        $this->schema()->drop('users');
-        $this->schema()->drop('subscriptions');
-
-    }
-
+    use SetupTrait;
 
     /**
     *  Tests...
@@ -316,27 +224,4 @@ class ChargeTest extends PHPUnit_Framework_TestCase
         $this->assertEquals( $updatedBillet['code'], 200 );
     }
 
-    /**
-    * Schema Helpers.
-    */
-    protected function schema()
-    {
-        return $this->connection()->getSchemaBuilder();
-    }
-
-    protected function connection()
-    {
-        return Eloquent::getConnectionResolver()->connection();
-    }
-
-    public function get_payment_token()
-    {
-        // Payment Token needs to be generated from Gerencianet
-        return false;
-    }
-}
-
-class User extends Eloquent
-{
-    use Laravel\Cashier\Billable;
 }
--- tests/SubscriptionTest.php
+++ tests/SubscriptionTest.php
@@ -1,36 +1,28 @@
 <?php
 
-use Carbon\Carbon;
-use Gerencianet\Exception\GerencianetException;
-use Gerencianet\Gerencianet;
-use Illuminate\Database\Capsule\Manager as DB;
-use Illuminate\Database\Eloquent\Model as Eloquent;
-use Illuminate\Http\Request;
-use Laravel\Cashier\GerencianetSubscription;
+use Laravel\Cashier\GerencianetSubscription as Subscription;
 
 class SubscriptionTest extends PHPUnit_Framework_TestCase
 {
 
-    private $options;
-    private $plans;
+    use SetupTrait;
 
-    private $user;
-
-    public static function setUpBeforeClass()
+    /**
+    *  Tests...
+    */
+    public function test_generic_user()
     {
-        if (file_exists(__DIR__.'/../.env')) {
-            $dotenv = new Dotenv\Dotenv(__DIR__.'/../');
-            $dotenv->load();
-        }
+        $this->assertEquals( 1, count( $this->user ) );
     }
 
-    public function setUp()
+    /**
+     * Test plan creation
+     */
+    public function test_create_plans()
     {
-        global $db, $user;
-        $this->user = $user;
-
+        // DONE:0 createPlans
         // Basic Plans
-        $GLOBALS['basicPlans'] = [
+        $basicPlans = [
             [
                 "name"     => "basic_monthly",
                 "interval" => 1,
@@ -54,7 +46,7 @@ class SubscriptionTest extends PHPUnit_Framework_TestCase
         ];
 
         // Pro plans
-        $GLOBALS['proPlans'] = [
+        $proPlans = [
             [
                 "name"     => "pro_monthly",
                 "interval" => 1,
@@ -77,85 +69,64 @@ class SubscriptionTest extends PHPUnit_Framework_TestCase
             ]
         ];
 
-
-    }
-
-    public function tearDown()
-    {
-
-        // $this->schema()->drop('users');
-        // $this->schema()->drop('subscriptions');
-
-    }
-
-
-    /**
-    *  Tests...
-    */
-    public function test_generic_user()
-    {
-        $this->assertEquals( 1, count( $this->user ) );
-    }
-
-    public function test_create_plans()
-    {
-        // DONE:0 createPlans
-        global $basicPlans, $proPlans;
-
-        $basic_plans = GerencianetSubscription::createPlan( $basicPlans );
+        $basic_plans = Subscription::createPlan( $basicPlans );
 
         $this->assertCount( 5, $basic_plans );
 
-        $pro_plans = GerencianetSubscription::createPlan( $proPlans );
+        $pro_plans = Subscription::createPlan( $proPlans );
 
         $this->assertCount( 5, $pro_plans );
     }
 
+    /**
+     *  Test list plans
+     */
     public function test_list_plans()
     {
-        // DONE:0 listPlans
-        $plans = GerencianetSubscription::listPlans();
+        // DONE:10 listPlans
+        $plans = Subscription::listPlans();
 
         $this->assertTrue( count( $plans ) >= 10 );
 
-        $basicPlans         = GerencianetSubscription::listPlans( "basic_" );
+        $basicPlans         = Subscription::listPlans( "basic_" );
         $basicPlans         = collect( $basicPlans )->pluck('name')->all();
         $basicPlansExpected = ["basic_monthly", "basic_bimonthly", "basic_quarterly", "basic_semiannual", "basic_yearly"];
 
         $this->assertEquals( count(array_intersect( $basicPlans, $basicPlansExpected)), count($basicPlans) );
 
-        $proPlans         = GerencianetSubscription::listPlans( "pro_" );
+        $proPlans         = Subscription::listPlans( "pro_" );
         $proPlans         = collect( $proPlans )->pluck('name')->all();
         $proPlansExpected = ["pro_monthly", "pro_bimonthly", "pro_quarterly", "pro_semiannual", "pro_yearly"];
 
         $this->assertEquals( count(array_intersect( $proPlans, $proPlansExpected)), count($proPlans) );
 
-
     }
-
-    // TODO:30 deletePlan
-    // TODO:20 createSubscription
-    // TODO:40 detailSubscription
-    // TODO:70 updateSubscriptioMetadata
-    // TODO:10 cancelSubscription
-    // TODO:60 paySubscription
 
     /**
-    * Schema Helpers.
-    */
-    protected function schema()
+     * Tests plan exclusion
+     */
+    public function teste_delete_plan()
     {
-        return $this->connection()->getSchemaBuilder();
+        // DONE:20 deletePlan
+        $plan = [
+            "name" => "plan_for_delete",
+        ];
+        $plan = Subscription::createPlan( $plan );
+
+        $this->assertTrue( !isset( $plan['error'] ) );
+        $this->assertTrue( isset( $plan['plan_id'] ) );
+        $this->assertEquals( "plan_for_delete", $plan['name'] );
+
+        $deletePlan = Subscription::deletePlan( $plan['plan_id'] );
+
+        $this->assertTrue( $deletePlan );
+
     }
 
-    protected function connection()
-    {
-        return Eloquent::getConnectionResolver()->connection();
-    }
+    // TODO:10 createSubscription
+    // TODO:20 detailSubscription
+    // TODO:30 updateSubscriptioMetadata
+    // TODO:40 cancelSubscription
+    // TODO:50 paySubscription
 
-    public function get_payment_token()
-    {
-        // Payment Token needs to be generated from Gerencianet
-        return false;
-    }
 }
